9dc7f361ad4263a59e3a915822e025ac9750fcfc
Luis Reyes <luisreyesxv@yahoo.com>
2020-08-28 02:14:52 -0400

    Done.

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -4,22 +4,42 @@ class LoginForm extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      username: "",
+      password: ""
+    };
   }
+
+  changeHandler = (event) =>{
+
+    this.setState({
+      ...this.state,
+      [event.target.name]: event.target.value
+
+    })
+  }
+
+  submitHandler = (event) => {
+    event.preventDefault()
+    if (this.state.username.length > 0 || this.state.password.length > 0){
+      this.props.handleLogin(this.state)
+    }
+  }
+
 
   render() {
     return (
-      <form>
+      <form onSubmit = {this.submitHandler}>
         <div>
           <label>
             Username
-            <input id="username" name="username" type="text" />
+            <input id="username" name="username" type="text" value={this.state.username} onChange={this.changeHandler} />
           </label>
         </div>
         <div>
           <label>
             Password
-            <input id="password" name="password" type="password" />
+            <input id="password" name="password" type="password" value={this.state.password} onChange={this.changeHandler} />
           </label>
         </div>
         <div>
--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -4,14 +4,27 @@ class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      twitterText: ""
+    };
+  }
+
+
+  changeHandler = (event) =>{
+    
+    this.setState({
+      twitterText: event.target.value
+    })
+
+
   }
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
+        <input type="text" name="message" id="message" value={this.state.twitterText} onChange ={this.changeHandler} />
+       {this.props.maxChars - this.state.twitterText.length}
       </div>
     );
   }
